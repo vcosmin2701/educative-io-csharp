8fd0cd5148b80e8d93c7d49af601f61bb88a1022
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-06-07 16:13:57 +0300

    Add project files.

---
+++ educative-io-csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using educative_io_csharp;
+class Program
+{
+	public static void Main(string[] args)
+	{
+		VendingMachine vend1 = new VendingMachine(true, 50, 10);
+		VendingMachine vend2 = new VendingMachine(true, 5);
+		VendingMachine vend3 = new VendingMachine();
+		Console.WriteLine("----------------------");
+		vend1.PrintFields();
+		Console.WriteLine("----------------------");
+		vend2.PrintFields();
+		Console.WriteLine("----------------------");
+		vend3.PrintFields();
+	}
+}
